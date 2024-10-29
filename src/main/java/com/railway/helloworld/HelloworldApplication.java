eaf02726a3f2fb47fde667de4177471321c00443
saisankar <saisankar3193@gmail.com>
2024-10-29 17:26:12 +0530

    update

--- src/main/java/com/railway/helloworld/HelloworldApplication.java
+++ src/main/java/com/railway/helloworld/HelloworldApplication.java
@@ -2,14 +2,23 @@ package com.railway.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
+@RestController
 public class HelloworldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloworldApplication.class, args);
 		System.out.println("The Spring Boot Project has Started");
+	}
+	
+	@GetMapping("/")
+	public String home()
+	{
+		return "<h1>Hello this is the starting project of Spring Boot</h1>";
 	}
 
 }
